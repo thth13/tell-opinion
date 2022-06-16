87c3b633d08554c01954cf2f23f66a749196151a
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2022-06-17 00:34:05 +0300

    fixes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const config = require('config');
 
 module.exports = function (req, res, next) {
   // Get token from header
@@ -10,7 +11,7 @@ module.exports = function (req, res, next) {
 
   // Verify token
   try {
-    jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
       if (error) {
         return res.status(401).json({ msg: 'Token is not valid' });
       } else {
