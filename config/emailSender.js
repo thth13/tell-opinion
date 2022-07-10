09eebde09432d016d82e5f69cc9403ffdb4f890e
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2022-07-10 14:33:37 +0300

    added email sender

---
+++ config/emailSender.js
@@ -0,0 +1,26 @@
+const nodemailer = require('nodemailer');
+
+const user = 'noreply@tell-opinion.com'
+const pass = process.env.EMAIL
+
+const transport = nodemailer.createTransport({
+  service: 'Outlook365',
+  auth: {
+    user,
+    pass
+  }
+})
+
+export const sendEmail = (subject, html, to) => {
+  transport.sendMail({
+    from: '"Tell Opinion" <noreply@tell-opinion.com>',
+    to,
+    subject,
+    html
+  }).catch(err => console.log(err));
+}
+
+// `<div><h1>Email Confirmation</h1>
+// <h2>Hello ${user.name}</h2>
+// <p>Thank you for subscribing. Please confirm your email by clicking on the following link</p>
+// </div>`,
