26657f82e44fc990dadf4672bc8eab3323851c6f
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2023-01-26 05:13:51 +0200

    added copy link

--- client/src/components/profile/Opinions/AdviceInMyProfile/AdviceInMyProfile.jsx
+++ client/src/components/profile/Opinions/AdviceInMyProfile/AdviceInMyProfile.jsx
@@ -1,9 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
+import copyIcon from "../../../../img/copy-icon.svg"
 import styles from "./styles.module.css"
 
 const AdviceInMyProfile = ({name, setIsShowAdviceInMyProfile, login}) => {
+  const [showCopiedTooltip, setShowCopiedTooltip] = useState(false);
+
   const closeAdvice = () => {
     setIsShowAdviceInMyProfile(false)
+  }
+
+  const copyLink = () => {
+    navigator.clipboard.writeText(`https://tell-opinion.com/${login}`)
+    setShowCopiedTooltip(true)
+    setTimeout(() => setShowCopiedTooltip(false), 3000);
   }
 
   return (
@@ -11,7 +20,13 @@ const AdviceInMyProfile = ({name, setIsShowAdviceInMyProfile, login}) => {
       <button className={styles.closeAdviceButton} onClick={closeAdvice}></button>
       {name && <span className={styles.adviceName}>{name}, </span>}
       {name ? 't' : 'T'}o receive opinions, share a link to your account in social networks
-      <span className={styles.link}>Your link: https://tell-opinion.com/{login}</span>
+      <span onClick={copyLink} className={styles.link}>
+        Your link: https://tell-opinion.com/{login}
+        <img className={styles.copyIcon} src={copyIcon} alt='Copy link icon' />
+      </span>
+        {showCopiedTooltip && 
+          <div className={styles.copiedTooltip}>Copied!</div>
+        }
     </div>
   )
 }
