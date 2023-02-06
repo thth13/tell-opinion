fe29597391e9c5ef81ddde97b0ff530e15175ad4
thth13 <dmitriy.khutornoy@cherrypiestudio.com>
2023-02-06 16:46:40 +0200

    autodetect language

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -4,10 +4,11 @@ import i18n from "i18next";
 import {initReactI18next} from "react-i18next";
 
 i18n.use(initReactI18next).use(LanguageDetector).use(Backend).init({
-    fallbackLng: "en",
+    fallbackLng: "ua",
+    whiteList: ['ua', 'en', 'ru'],
     detection: {
-      order: ['localStorage', 'cookie'],
-      cache: ['localStorage', 'cookie']
+      order: ['localStorage', 'cookie', 'navigator'],
+      cache: ['localStorage', 'cookie', 'navigator']
     },
     interpolation: {
       escapeValue: false
