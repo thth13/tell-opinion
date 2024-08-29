84309882cf28fa98c169194dc33d592ba0d1b8d9
Dmitriy Khutornoy <thth13q@gmail.com>
2024-08-30 02:09:59 +0300

    navbar selected style

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import { connect } from "react-redux"
+import { Link, useLocation } from 'react-router-dom'
+import c from 'classnames'
 import styles from './styles.module.css'
 import homeIcon from '../../img/homenav.svg'
 import mailIcon from '../../img/mailnav.svg'
 import searchIcon from '../../img/searchnav.svg'
-import noAvatar from "../../img/noAvatar.png"
-import { Link } from 'react-router-dom'
+import noAvatar from '../../img/noAvatar.png'
+
 
 const Navbar = ({profile, user, numberNotifications}) => {
+  const location = useLocation();
+
   return (
     <div className={styles.root}>
-      <Link to={'/'} className={styles.kek}>
+      <Link 
+        to={'/'}
+        className={c(styles.menuItem, {[styles.selectedItem]: location.pathname === '/'})}
+      >
         <button className={styles.button}>
           <img src={homeIcon} className={styles.icon} />
         </button>
       </Link>
-      <Link to={'/notifications'}>
+      <Link
+        to={'/notifications'}
+        className={c(styles.menuItem, {[styles.selectedItem]: location.pathname === '/notifications'})}
+      >
         <button className={styles.button}>
           {numberNotifications > 0 &&
             <div className={styles.numberNotifications}>{numberNotifications}</div>
@@ -23,12 +33,18 @@ const Navbar = ({profile, user, numberNotifications}) => {
           <img src={mailIcon} className={styles.icon} />
         </button>
       </Link>
-      <Link to={'/find'}>
+      <Link
+        to={'/find'}
+        className={c(styles.menuItem, {[styles.selectedItem]: location.pathname === '/find'})}
+      >
         <button className={styles.button}>
           <img src={searchIcon} className={styles.icon} />
         </button>
       </Link>
-      <Link to={`/${user?.login}`}>
+      <Link
+        to={`/${user?.login}`}
+        className={c(styles.menuItem, {[styles.selectedItem]: location.pathname === `/${user?.login}`})}
+      >
         <button className={styles.button}>
           <img
             src={profile?.avatar ? `https://spaces.tell-opinion.com/${profile.avatar}` : noAvatar}
