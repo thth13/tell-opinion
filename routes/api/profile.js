3b6be5aa5ac168aeef2ae94089a3b5b6e0b8088d
Dmitriy Khutornoy <thth13q@gmail.com>
2024-09-01 21:28:18 +0300

    heroku

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -14,7 +14,7 @@ const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 const Opinion = require('../../models/Opinion')
 
-const upload = multer({ dest: process.cwd() })
+const upload = multer({ dest: 'uploads/' })
 
 const opinionsLimit = 10
 
