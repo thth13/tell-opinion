af98619374a9202d2d1a4aacefb13fe15e9868fc
Dmitriy Khutornoy <thth13q@gmail.com>
2024-09-01 03:24:36 +0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const connectDB = require('./config/db')
 // const bodyParser = require('body-parser');
 const path = require('path')
+process.env['NODE_CONFIG_DIR'] = path.join(path.resolve("./"),"config/")
+
 
 const app = express()
 
